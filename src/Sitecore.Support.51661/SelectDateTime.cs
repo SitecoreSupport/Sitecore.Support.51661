7c268d9c695a9948ae4b3f7d43a31f009763dde1
DK\dmko <dmko@sitecore.net>
2017-12-26 12:46:46 +0200

    Сonsiders the configured region format of date and time in the Preview Date and Time dialog box

--- src/Sitecore.Support.51661/SelectDateTime.cs
+++ src/Sitecore.Support.51661/SelectDateTime.cs
@@ -1,30 +1,26 @@
-﻿using Sitecore.Diagnostics;
-using Sitecore.Mvc.Presentation;
-using Sitecore.Web.PageCodes;
+﻿using Sitecore.Mvc.Presentation;
+using Sitecore.StringExtensions;
 using System.Globalization;
 
-namespace Sitecore.ExperienceEditor.Speak.Dialogs
+namespace Sitecore.Support.ExperienceEditor.Speak.Dialogs
 {
-  public class SelectDateTime : PageCodeBase
+  public class SelectDateTime : Sitecore.ExperienceEditor.Speak.Dialogs.SelectDateTime
   {
     private const string IsAmPmFormatParameter = "IsAMPMFormat";
 
-    public Rendering SelectedTime
-    {
-      get;
-      set;
-    }
+    public Rendering SelectedDate { get; set; }
 
     public override void Initialize()
     {
-      this.InitializeIsAmPmFormat();
-    }
-
-    private void InitializeIsAmPmFormat()
-    {
-      CultureInfo culture = Context.User.Profile.Culture;
-      Assert.IsNotNull(culture, "Culture info iis null.");
-      this.SelectedTime.Parameters["IsAMPMFormat"] = (!string.IsNullOrEmpty(culture.DateTimeFormat.AMDesignator) && !string.IsNullOrEmpty(culture.DateTimeFormat.PMDesignator)).ToString();
+      base.Initialize();
+      if (this.SelectedDate.Parameters["Format"].IsNullOrEmpty())
+      {
+        CultureInfo culture = Context.User.Profile.Culture;
+        if (culture != null && culture.DateTimeFormat != null)
+        {
+          this.SelectedDate.Parameters["Format"] = culture.DateTimeFormat.ShortDatePattern;
+        }
+      }
     }
   }
 }
