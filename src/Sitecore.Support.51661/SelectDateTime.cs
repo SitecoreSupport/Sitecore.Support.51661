7427cdae535ed74a70f58d0f75ecfd0e2566bc3c
DK\dmko <dmko@sitecore.net>
2017-12-26 12:28:22 +0200

    Original code

---
+++ src/Sitecore.Support.51661/SelectDateTime.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.PageCodes;
+using System.Globalization;
+
+namespace Sitecore.ExperienceEditor.Speak.Dialogs
+{
+  public class SelectDateTime : PageCodeBase
+  {
+    private const string IsAmPmFormatParameter = "IsAMPMFormat";
+
+    public Rendering SelectedTime
+    {
+      get;
+      set;
+    }
+
+    public override void Initialize()
+    {
+      this.InitializeIsAmPmFormat();
+    }
+
+    private void InitializeIsAmPmFormat()
+    {
+      CultureInfo culture = Context.User.Profile.Culture;
+      Assert.IsNotNull(culture, "Culture info iis null.");
+      this.SelectedTime.Parameters["IsAMPMFormat"] = (!string.IsNullOrEmpty(culture.DateTimeFormat.AMDesignator) && !string.IsNullOrEmpty(culture.DateTimeFormat.PMDesignator)).ToString();
+    }
+  }
+}
